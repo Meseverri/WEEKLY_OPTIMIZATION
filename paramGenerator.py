b65172e6a28a8250c5e85268b3e1c26d7f84ee7a
Meseverri <66443952+Meseverri@users.noreply.github.com>
2022-09-30 13:22:57 +0200

    30-09-2022 Param generator V 0.1

---
+++ paramGenerator.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def normOfDiference(list1,list2):
+    arr1=np.array(list1)
+    arr2=np.array(list2)
+    dif=np.subtract(arr1,arr2)
+    return round(np.linalg.norm(dif),3)
+
+allBestData_df=pd.read_excel("BestMedianResults.xlsx",index_col=0)
+
+
+
+
+
+data={}
+for ref in range(len(allBestData_df.columns)):   
+    fila=[]
+    list1=allBestData_df.iloc[-8:,ref].to_list()
+    rowName=allBestData_df.iloc[-8:,ref].name
+    for i in range(len(allBestData_df.columns)):
+        list2=allBestData_df.iloc[-8:,i].to_list()
+        fila.append(normOfDiference(list1,list2))
+    data[rowName]=fila
+
+relationMatrix=pd.DataFrame(data,index=allBestData_df.columns)
+mIndex=[]
+for i in relationMatrix.columns.to_list():
+    mIndex.append((int(i[:4]),int(i[5:])))
+
+# 
+index = pd.MultiIndex.from_tuples(mIndex, names=["Year", "Week"])
+
+
+RelationMatrix=pd.DataFrame(relationMatrix.values,index,columns=index)
+AllBestData=pd.DataFrame(allBestData_df.values,allBestData_df.index,columns=index)
+    
+"""Usaremos multiples estrategias de eleccion y estimacion de parametros
+        E1: ubservamos el entorn al rededor de la semana +-2 respecto a la sem de referencia
+        E2: tomamos 4 hacia atra
+        E3: comparando todas las del año
+        E3: comparando 53 semanas hacia atras"""
+
