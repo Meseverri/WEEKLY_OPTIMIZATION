a71e8557121cc443cfeec34029cfad2f7e778074
Meseverri <66443952+Meseverri@users.noreply.github.com>
2022-10-06 10:43:34 +0200

    2015 almost all xml

--- test.ps1
+++ test.ps1
@@ -1,7 +1,13 @@
 cd "C:\Program Files\MetaTrader 5"
 $ProyectPath = "C:\Users\mesev\Documents\BackUp\TRABAJO\FUDO\FUDO TECHNOLOGIES\Backtest MT5\EURUSD\WEEKLY_OPTIMIZATION"
-Get-ChildItem -Path "$($ProyectPath)\AllOpt" -Recurse -Filter *.ini | 
+$fileArray = Get-ChildItem -Path "$($ProyectPath)\AllOpt" -Recurse -Filter *.ini 
+
+$totalfiles = $fileArray.length
+$i = 1
+$percent = [math]::round($i/$totalfiles, 5)
+$fileArray |
 Foreach-Object {
+Write-Progress -Activity "Optimizaciones en Proceso " -Status "$percent% Complete:" -PercentComplete $percent
 $year = $_.Name.Substring(25,4)
 $yearWeek = $_.Name.Substring(25,$_.Name.length-4-25)
 Write-Host $yearWeek
@@ -11,7 +17,13 @@ while (!(Test-Path $($optResult))) {
 Start-Sleep 30 
 }
 Move-Item -Path $($optResult)  -Destination "$($ProyectPath)\$($year)"  -PassThru
-Write-Host "Se ha movido el archivo:\n$($optResult)\n A la carpeta:\n$($ProyectPath)\$($year)"
+Write-Host "Se ha movido el archivo:"
+Write-Host "$($optResult)"
+Write-Host "A la carpeta:"
+Write-Host "$($ProyectPath)\$($year)"
+Write-Host "-----------------------------------------------------------------------------------------------"
+$i++ 
+$percent = [math]::round($i/$totalfiles, 5)
 }
 
 Write-Host "Todos los .ini ejecutados"
