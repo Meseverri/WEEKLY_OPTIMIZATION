1e3743e74958c2aecd642b4c1b6eb838b3a164c1
gerardohb99 <gerardohb99@gmail.com>
2022-10-11 12:19:34 +0200

    flata probar GenerateAllBacktesting

--- GenerateAllBacktesting.ps1
+++ GenerateAllBacktesting.ps1
@@ -1,5 +1,9 @@
-cd "C:\Program Files\MetaTrader 5"
-$ProyectPath = "C:\Users\mesev\Documents\BackUp\TRABAJO\FUDO\FUDO TECHNOLOGIES\Backtest MT5\EURUSD\WEEKLY_OPTIMIZATION"
+$jsonPath = Get-Content 'D:\FUDO TECH\WEEKLY_OPTIMIZATION\confPath.json' | Out-String | ConvertFrom-Json
+$terminalPath = $jsonPath.terminalPath
+$ProyectPath = $jsonPath.proyectPath
+$mt5OutPutPath  = $jsonPath.mt5OutPutPath
+cd $terminalPath
+
 $fileArray = Get-ChildItem -Path "$($ProyectPath)\2021_BT_ini" -Recurse -Filter *.ini 
 
 $totalfiles = $fileArray.length
@@ -12,10 +16,10 @@ Foreach-Object {
     $yearWeek = $_.Name.Substring(27,$_.Name.length-4-27)
     Write-Host $yearWeek
 
-    Copy-Item -Path "$($ProyectPath)\2021_BT_sets\$($yearWeek.Substring(4)).set" -Destination "C:\Users\mesev\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Profiles\Tester"
+    Copy-Item -Path "$($ProyectPath)\2021_BT_sets\$($yearWeek.Substring(4)).set" -Destination "$($mt5OutPutPath)\MQL5\Profiles\Tester"
     if (!(Test-Path "$($ProyectPath)\$($year)_BT_results\$($yearWeek).htm") ) {
         .\terminal64.exe /config:$($_.FullName)
-        $btResult = "C:\Users\mesev\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\$($yearWeek).htm"
+        $btResult = "$($mt5OutPutPath)\$($yearWeek).htm"
         while (!(Test-Path $($btResult))) { 
             Start-Sleep 10 
         }
@@ -25,7 +29,7 @@ Foreach-Object {
         Write-Host "A la carpeta:"
         Write-Host "$($ProyectPath)\$($year)_BT_results"
     }
-    Remove-Item "C:\Users\mesev\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Profiles\Tester\$($yearWeek.Substring(4)).set"
+    Remove-Item "$($mt5OutPutPath)\MQL5\Profiles\Tester\$($yearWeek.Substring(4)).set"
     Write-Host "-----------------------------------------------------------------------------------------------"
     $i++ 
     $percent = [math]::round($i/$totalfiles, 5)
